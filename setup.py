3f9fd67282d1c135016f3b0721a08f598d80e656
achamaria2701 <ankitchamaria1@gmail.com>
2024-09-02 17:01:20 +0530

    Requirements Added & Installation done

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarizer"
+AUTHOR_USER_NAME = "achamaria2701"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "ankitchamaria1@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Python package for NLP applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_url={
+        "BugTracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
+)
--- template.py
+++ template.py
@@ -23,7 +23,7 @@ list_of_files = [
     "app.py",
     "main.py",
     "Dockerfile",
-    "requirements.py",
+    "requirements.txt",
     "setup.py",
     "research/trials.ipynb"
 ]
